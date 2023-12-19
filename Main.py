9c9f0ce782836adcc07464644721eeff5076d62a
Ali Rezaei <alii.rezzaei@gmail.com>
2023-12-19 10:33:54 +0330

    Main

--- Main.py
+++ Main.py
@@ -3,8 +3,9 @@ import numpy as np
 from utils.Plotter import Plotter
 
 Pendy = InvPendulum()
-Sol, t = Pendy.Simulate(x0=np.random.rand(4), t=np.linspace(0, 20, 1000))
+Sol, t = Pendy.Simulate(x0=10*np.random.rand(4), t=np.linspace(0, 20, 1000))
 
 Plt = Plotter(t, Sol)
 Plt.PlotResults()
 print(f"System Steady State: {Sol[-1,]}")
+ 
